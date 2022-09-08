643c04620a9a47d01dcb2b87942b48599f835b5d
Fidan Oruc <fidanorucc>
2022-09-08 10:37:51 +0200

    Initial Commit

---
+++ VideoListApp/VideoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoListAppApp.swift
+//  VideoListApp
+//
+//  Created by Fidan Oruc on 08.09.22.
+//
+
+import SwiftUI
+
+@main
+struct VideoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
